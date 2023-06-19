a882479c67738fb69ba40f0c156f68821fb9439b
oliver-ruan <ruanboyu98@gmail.com>
2023-06-19 13:10:39 +1000

    Saved url

--- NBA_PredictPlayerSalary/NBA_PredictPlayerSalary_Script.R
+++ NBA_PredictPlayerSalary/NBA_PredictPlayerSalary_Script.R
@@ -26,3 +26,10 @@ AllPlayerSlugs <- read.csv('C:/Users/ruanb/OneDrive/Documents/NBA_Analytics/NBA_
 
 schedule <- url %>% read_html %>%  html_node('table#totals') %>% html_table() 
 # https://stackoverflow.com/questions/69007853/error-in-usemethodhtml-table-no-applicable-method-for-html-table-applied
+
+# Data 1998-2022
+# https://data.world/etocco/nba-player-stats
+
+# Data 1950-2017
+# https://www.kaggle.com/datasets/whitefero/nba-players-advanced-season-stats-19782016
+
